74a460245ecc06177a3d322b9dd062569cad5545
Lucas Guillerot lguiller <lguiller@student.42.fr>
2019-12-02 17:37:34 +0100

    optimisation sur le return de calc_blur -6sec blur kernelsize 2

--- day03/ex04/AdvancedFilter.py
+++ day03/ex04/AdvancedFilter.py
@@ -3,7 +3,7 @@ from loading import ft_progress
 
 def calc_blur(array, kernel):
     tmp_array = array * kernel
-    return (np.sum(np.sum(tmp_array, axis = 1), axis = 0) / (np.sum(kernel) / 3 + 0.00001))
+    return (tmp_array.sum(1).sum(0) / (np.sum(kernel[:,:,0]) + 0.00001))
 
 def browse_pixels(array, kernel, size):
     new_array = np.zeros(np.shape(array))
@@ -16,7 +16,7 @@ def browse_pixels(array, kernel, size):
             gapy = [np.abs(y - size) if (y - size < 0) else 0, -(y + size - length + 1) if (y + size >= length - 1) else 0]
 
             resize_kernel = kernel[gapy[0] if (gapy[0]) else None:gapy[1] if (gapy[1]) else None, gapx[0] if (gapx[0]) else None:gapx[1] if (gapx[1]) else None]
-            new_array[y, x] = calc_blur(array[y - size + gapy[0]:y + size + 1 + gapy[1], x - size + gapx[0] : x + size + 1 + gapx[1]], resize_kernel)
+            new_array[y, x] = calc_blur(array[y - size + gapy[0]:y + size + 1 + gapy[1], x - size + gapx[0]:x + size + 1 + gapx[1]], resize_kernel)
     print()
     return (new_array)
 
